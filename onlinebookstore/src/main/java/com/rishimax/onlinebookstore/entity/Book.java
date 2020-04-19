57f7edcf04d42b6b1f9303dabf4edfd0ef7dfa46
Rishimaxx <Rishimaxx@DESKTOP-7G74I8Q>
2020-04-19 10:40:21 +0530

    Created angular project and completed the feature 1

--- onlinebookstore/src/main/java/com/rishimax/onlinebookstore/entity/Book.java
+++ onlinebookstore/src/main/java/com/rishimax/onlinebookstore/entity/Book.java
@@ -12,15 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 @Entity
 @Table(name="tbl_book")
-@Getter
-@Setter
-@ToString
 public class Book {
 	
 	@Id
@@ -53,4 +46,101 @@ public class Book {
 	@ManyToOne
 	@JoinColumn(name = "category_id", nullable = false)
 	private BookCategory category;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public int getUnitsInStck() {
+		return unitsInStck;
+	}
+
+	public void setUnitsInStck(int unitsInStck) {
+		this.unitsInStck = unitsInStck;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public BookCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(BookCategory category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", sku=" + sku + ", name=" + name + ", description=" + description + ", unitPrice="
+				+ unitPrice + ", imageUrl=" + imageUrl + ", active=" + active + ", unitsInStck=" + unitsInStck
+				+ ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + ", category=" + category + "]";
+	}
+	
+	
 }
